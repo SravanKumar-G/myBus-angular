742a9c7a9e5d8de99d96b601f818ee1df5d2a13c
SravanKumar-G <sravang432@gmail.com>
2021-09-24 09:51:30 +0530

    cargoCancellation added

--- src/app/_helpers/apiUrls.ts
+++ src/app/_helpers/apiUrls.ts
@@ -6,6 +6,13 @@ import {Injectable} from '@angular/core';
 
 export class ApiUrls {
   mainUrl = 'http://localhost:8091/';
+  // mainUrl = 'http://cargo.srikrishnatravels.in/';
 
   getCurrentUser = 'api/v1/user/me';
+
+  pendingShipmentsCount = 'api/v1/shipment/count/cancellationPendingShipments';
+  getAllPendingShipments = 'api/v1/shipment/get/pendingShipments';
+  cancelledShipmentsCount = 'api/v1/shipment/count/cancelled';
+  getAllCancelledShipments = 'api/v1/shipment/search/cancelled';
+
 }
