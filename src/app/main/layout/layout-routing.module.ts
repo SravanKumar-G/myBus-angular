372ae344d752acee950f9f3c5df8501bcb8a426b
srinikandula <srini.kandula@whizzard.in>
2021-12-01 23:28:29 +0530

    Adding comments display

--- src/app/main/layout/layout-routing.module.ts
+++ src/app/main/layout/layout-routing.module.ts
@@ -121,16 +121,11 @@ const layOutRoutes: Routes = [
             //         .then(m => m.ServiceReportByServiceModule)
             // },
             {
-                path: 'serviceReportByService/:id',
-                loadChildren: () => import('./reports/service-report-by-service/service-report-by-service.module').
-                then(m => m.ServiceReportByServiceModule)
-            },
-            {
                 path: 'maintenance',
                 loadChildren: () => import('./maintenance/maintenance.module').
                 then(m => m.MaintenanceModule)
             },
-                path: 'agents',
+            {    path: 'agents',
                 loadChildren: () => import('./features/agents/agents.module')
                     .then(m => m.AgentsModule)
             },
